898fba593be925e02662670f3e1b3d289432d382
merzaad <merzaad@gmail.com>
2023-07-18 16:01:13 +0330

    store

--- src/routes/store/+page.svelte
+++ src/routes/store/+page.svelte
@@ -1,23 +1,51 @@
 <script>
+	import { onDestroy, onMount } from 'svelte';
 	import Box from '../../components/Box.svelte';
 	import Button from '../../components/Button.svelte';
-	import { someStore } from '../../stores/stores';
+	import {
+		someStore,
+		someOtherStore,
+		timerStore,
+		otherTimerStore,
+		customStore,
+		inputStore
+	} from '../../stores/stores';
 	/**
 	 * @type {number}
 	 */
 	let storedValue;
-	someStore.subscribe((value) => {
+	const unsubscribe = someOtherStore.subscribe((value) => {
 		storedValue = value;
 	});
-	const updateStore = () => {
-		someStore.update((value) => value + 1);
+	const updateSomeOtherStore = () => {
+		someOtherStore.update((value) => value + 1);
 	};
+	onDestroy(unsubscribe);
 </script>
 
 <div class="store">
 	<Box>
-		stored: {storedValue}
-		<Button on:click={updateStore}>store+1</Button>
+		unsubscribe onDestory: {$someStore}
+		<Button on:click={() => $someStore++}>+1</Button>
+	</Box>
+	<Box>
+		auto unsubscribe: {storedValue}
+		<Button on:click={updateSomeOtherStore}>+1</Button>
+	</Box>
+	<Box>
+		readable: {$timerStore}
+	</Box>
+	<Box>
+		derived: {$otherTimerStore}
+	</Box>
+	<Box>
+		custom store: {$customStore}
+		<Button on:click={customStore.increase}>+1</Button>
+		<Button on:click={customStore.reset}>reset</Button>
+	</Box>
+	<Box>
+		binded store {$inputStore}
+		<input bind:value={$inputStore} />
 	</Box>
 </div>
 
@@ -28,5 +56,6 @@
 		justify-content: center;
 		align-items: center;
 		flex-direction: column;
+		gap: 1rem;
 	}
 </style>
--- src/stores/stores.js
+++ src/stores/stores.js
@@ -1,3 +1,19 @@
-import { writable } from 'svelte/store';
+import { derived, readable, writable } from 'svelte/store';
 
 export const someStore = writable(0);
+export const someOtherStore = writable(0);
+export const timerStore = readable(new Date().toLocaleTimeString(), (set) => {
+	const interval = setInterval(() => {
+		set(new Date().toLocaleTimeString());
+	}, 1000);
+	return () => clearInterval(interval);
+});
+export const otherTimerStore = derived(timerStore, (timer) => timer + ' derived');
+
+const createCustomeStore = () => {
+	const { subscribe, set, update } = writable(0);
+	return { subscribe, increase: () => update((prev) => prev + 1), reset: () => set(0) };
+};
+
+export const customStore = createCustomeStore();
+export const inputStore = writable(0);
